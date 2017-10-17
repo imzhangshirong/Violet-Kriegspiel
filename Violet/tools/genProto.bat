9fa05dbe441bcd2a17b0d42bd6f2631c9f5d332d
longwei1 <2793361740@qq.com>
2017-10-17 16:50:16 +0800

    tool

---
+++ Violet/tools/genProto.bat
@@ -0,0 +1,12 @@
+#--plugin=protoc-gen-grpc=grpc_csharp_plugin.exe
+
+set PROTOCNODE=protoc --js_out=import_style=commonjs,binary:../servernode/proto   --proto_path=../protos
+
+set PROTOCCSHARP=protoc --csharp_out=../client\Assets\Plugins\Proto  --proto_path=../protos 
+
+set PROTOCJAVA=protoc --java_out=../serverjava/ensign/src/main/java  --proto_path=../protos
+
+%PROTOCNODE% Client.proto
+%PROTOCCSHARP% Client.proto
+%PROTOCJAVA% Client.proto
+pause
