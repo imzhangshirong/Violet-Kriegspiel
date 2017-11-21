309dbc69fcc8760db42ab6872dd90fc2b4bdfedc
Jarvis Zhang <345054597@qq.com>
2017-11-21 14:47:39 +0800

    node add & login

--- servernode/server.js
+++ servernode/server.js
@@ -566,10 +566,10 @@ RpcServer.on("Login",function(requestData){
             roundOrder:0,
             chessSetting:"6|1,3|2,0|3,11|4,0|5;10|6,2|7,5|8,0|9,3|10;9|11,,2|12,,4|13;1|14,5|15,,6|16,7|17;7|18,,4|19,,1|20;8|21,2|22,4|23,3|24,8|25;"
         };
+        user.token = requestData.token;
         UserData.push(user);
         let PlayerInfo = RpcServer.getRpc("PlayerInfo","");
         let playerInfo = getRpcPlayerInfo(user);
-        user.token = requestData.token;
         console.log("add & login:"+user.userName+":"+user.token);
         response.setPlayerinfo(playerInfo);
         //requestData.errorCode = 1;
